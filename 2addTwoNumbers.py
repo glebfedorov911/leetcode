10c96b00601fbd8244a85fc8063f5adc3825cad7
glebfedorov911 <fedorov22134@mail.ru>
2024-01-06 11:15:05 +0300

    new solution for ex 4, 5
    Signed-off-by: glebfedorov911 <fedorov22134@mail.ru>

--- 2addTwoNumbers.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional, List
-
-class Solution:
-    def addTwoNumbers(self, l1: Optional[List], l2: Optional[List]) -> Optional[List]:
-        return [i for i in str(int(''.join(list(map(str, l1)))[::-1])+int(''.join(list(map(str, l2)))[::-1]))[::-1]]
-
-s = Solution()
-print(s.addTwoNumbers([2,4,3], [5,6,4])) # with List Work
